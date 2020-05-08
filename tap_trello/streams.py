c5384b13f2396932d51d9179d33db368ea5002f5
Jacob Baca <jbaca@stitchdata.com>
2020-05-08 18:43:14 +0000

    WIP refactoring actions bookmarks

--- tap_trello/streams.py
+++ tap_trello/streams.py
@@ -1,5 +1,6 @@
 import singer
-import datetime
+from datetime import datetime, timedelta
+from itertools import dropwhile
 
 from singer import utils
 
@@ -71,36 +72,79 @@ class DateWindowPaginated(Mixin):
     """
     Mixin class to provide date windowing on the `get_records` requests
     """
-    # Default, can be overridden in implementation, the API support fractional days
-    date_window = 30
-    bookmark_key = 'next_window_start'
+    sync_start = None
+    def _get_window_state(self):
+        window_state = {}
 
-    def _get_initial_bookmark(self):
-        bookmark = singer.get_bookmark(self.state, self.stream_id, self.bookmark_key)
-        if not bookmark:
-            return utils.strptime_to_utc(self.config['start_date'])
+        start_date = self.config['start_date']
+        window_start_bookmark = singer.get_bookmark(self.state, self.stream_id, 'window_start') or start_date
+        window_start = utils.strptime_to_utc(max(window_start_bookmark, start_date))
 
-        return utils.strptime_to_utc(bookmark)
+        window_sub_end = singer.get_bookmark(self.state, self.stream_id, 'window_sub_end')
+        window_sub_end = window_sub_end and utils.strptime_to_utc(window_sub_end)
 
-    def update_bookmark(self, bookmark_value):
-        singer.bookmarks.write_bookmark(
-            self.state, self.stream_id, self.bookmark_key, bookmark_value
-        )
-        singer.write_state(self.state)
+        # NB: window_end identifies either the first window's end_date, or the next window's start_date
+        # - If it's not present, this indicates that we're on our first sync.
+        next_window_start = singer.get_bookmark(self.state, self.stream_id, 'next_window_start')
+        next_window_start = next_window_start and utils.strptime_to_utc(next_window_start)
 
+        if window_sub_end is not None and next_window_start is None:
+            raise Exception("Invalid State: window_sub_end provided without a next_window_start.")
+
+        return window_start, window_sub_end, next_window_start
+
+    def sync_started(self):
+        # Create a sync_start value here, store in state
+    
+    def sync_finished(self):
+        # Upgrade sync_start to a bookmark here to be used next time through to override `start_date`.
+
+    
     def get_records(self, format_values, params=None):
+        """ Overrides the default get_records to provide date_window pagination and bookmarking. """
+        # TODO: Pre-task to check if there is a "now" written already and use the min?
         if params is None:
             params = {}
 
-        # NB: Both request parameters are exclusive, so this makes the windowing inclusive
-        # - The Trello API doesn't appear to go to microsecond precision
-        window_start = self._get_initial_bookmark() - datetime.timedelta(milliseconds=1)
-        end = utils.now() + datetime.timedelta(milliseconds=1)
+        self.sync_start = utils.strptime(singer.get_bookmark(self.state, self.stream_id, 'sync_start', utils.strftime(utils.now())))
+        window_start, window_sub_end, next_window_start = self._get_window_state()
+        window_start -= timedelta(milliseconds=1) # To make start inclusive
 
-        window_offset = datetime.timedelta(days=self.date_window)
-        window_end = window_start + window_offset + datetime.timedelta(milliseconds=1)
-        initial_end = window_end # Initial end used for resuming post-sub-paginating
-        while window_start < end:
+        if window_sub_end is not None:
+            # Indicates we need to resume, next_window_start should be present
+            next_window_start -= timedelta(milliseconds=1) # To make next start inclusive
+            for rec in self._paginate_window(window_start, window_sub_end, format_values, params):
+                yield rec
+
+        # Sync records since previous window
+        # next_window_start indicates we resumed a sync, if not present, then we use window_start
+        next_window_start = next_window_start or window_start
+
+        now = utils.strftime(self.sync_start)
+        next_window_end = utils.strptime_to_utc(min(self.config.get('end_date', now), now))
+
+        self._update_bookmark("next_window_start", next_window_end)
+
+        for rec in self._paginate_window(next_window_start, next_window_end, format_values, params):
+            yield rec
+
+        # Set the final end to the next start
+        self._update_bookmark("window_start", next_window_end)
+        singer.bookmarks.clear_bookmark(self.state, self.stream_id, "next_window_start")
+        singer.write_state(self.state)
+
+
+    def _update_bookmark(self, key, value):
+        singer.bookmarks.write_bookmark(
+            self.state, self.stream_id, key, utils.strftime(value)
+        )
+
+    def _paginate_window(self, window_start, window_end, format_values, params):
+        self._update_bookmark("window_start", window_start)
+        self._update_bookmark("window_sub_end", window_end)
+        singer.write_state(self.state)
+        
+        while True:
             records = self.client.get(self._format_endpoint(format_values), params={"since": utils.strftime(window_start),
                                                                                     "before": utils.strftime(window_end),
                                                                                     **params})
@@ -116,18 +160,14 @@ class DateWindowPaginated(Mixin):
                 # NB: Actions are sorted backwards, so if we get the
                 # max_response_size, set the window_end to the last
                 # record's timestamp (inclusive) and try again.
-                window_end = utils.strptime_to_utc(records[-1]["date"]) + datetime.timedelta(milliseconds=1)
+                window_end = utils.strptime_to_utc(records[-1]["date"]) + timedelta(milliseconds=1)
+                self._update_bookmark("window_sub_end", window_end)
+                singer.write_state(self.state)
             else:
-                if window_end < end:
-                    self.update_bookmark(utils.strftime(window_end))
-                elif window_end >= end:
-                    self.update_bookmark(utils.strftime(end))
-
-                window_start = initial_end - datetime.timedelta(milliseconds=1)
-                window_end = window_start + window_offset + datetime.timedelta(milliseconds=1)
-                initial_end = window_end
-
-            # TODO: Store both start/end bookmarks here?
+                singer.bookmarks.clear_bookmark(self.state, self.stream_id, "window_start")
+                singer.bookmarks.clear_bookmark(self.state, self.stream_id, "window_sub_end")
+                singer.write_state(self.state)
+                break
 
 
 class Stream:
@@ -177,14 +217,45 @@ class ChildStream(Stream):
         for parent_obj in parent.get_records(parent.get_format_values(), params={"fields": "id"}):
             yield parent_obj['id']
 
+    def _sort_parent_ids_by_created(self, parent_ids):
+        # NB This is documented here. Yes it's hacky
+        # - https://help.trello.com/article/759-getting-the-time-a-card-or-board-was-created
+        parents = [{"id": x, "created": datetime.utcfromtimestamp(int(x[:8], 16))}
+                   for x in parent_ids]
+        return sorted(parents, key=lamba x: x["created"])
+        
     # TODO: If we need second-level child streams, most of sync needs pulled into get_records for this class
 
     def sync(self):
         parent = self.parent_class(self.client, self.config, self.state)
-        for parent_id in self.get_parent_ids(parent):
-            # Get users for "parent_id" (aka board_id)
+
+        # Ensure that start time of this stream's sync is bookmarked
+        sync_start = utils.strptime(singer.get_bookmark(self.state, self.stream_id, 'sync_start', utils.strftime(utils.now())))
+        singer.write_bookmark(self.state, self.stream_id, 'sync_start', utils.strftime(sync_start))
+        singer.write_state(self.state)
+
+        # Get the most recent parent ID and resume from there, if necessary
+        bookmarked_parent = singer.get_bookmark(self.state, self.stream_id, 'parent_id')
+        parent_ids = [p['id'] for p in self._sort_parent_ids_by_created(self.get_parent_ids(parent))]
+
+        if bookmarked_parent and bookmarked_parent in parent_ids:
+            # NB: This will cause some rework, but it will guarantee the tap doesn't miss records if interrupted.
+            # - If there's too much data to sync all parents in a single run, this API is not appropriate for that data set.
+            parent_ids = dropwhile(lambda p: parent_id != bookmarked_parent,
+                                  parent_ids)
+        for parent_id in parent_ids:
+            # --- Clear State
+            # --- Add parent_id for next time
+            # Clear all but sync_start and parent_id
+            singer.write_bookmark(self.state, self.stream_id, "parent_id", parent_id)
             for rec in self.get_records([parent_id]):
                 yield rec
+        singer.clear_bookmark(self.state, self.stream_id, "sync_start")
+        singer.clear_bookmark(self.state, self.stream_id, "parent_id")
+        self.sync_finished()
+        # -- Write child's bookmark to "now", maybe ask self what that means?
+        # - e.g., self.write_bookmark_to_now() or something similar where we write the "earliest now that was written"
+        # -- And remove the parent_id bookmark (we're done)
 
 
 
