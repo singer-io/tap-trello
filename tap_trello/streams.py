0ec4c712e9e930bee1b2fd03665defb148429dae
Jacob Baca <jbaca@stitchdata.com>
2020-05-27 14:17:00 +0000

    added support for dropdown fields

--- tap_trello/streams.py
+++ tap_trello/streams.py
@@ -193,14 +193,14 @@ class Stream:
     def modify_record(self, record, **kwargs): # pylint: disable=no-self-use,unused-argument
         return record
 
-    def build_custom_fields_map(self, **kwargs): # pylint: disable=no-self-use,unused-argument
-        return {}
+    def build_custom_fields_maps(self, **kwargs): # pylint: disable=no-self-use,unused-argument
+        return {}, {}
 
     def get_records(self, format_values, additional_params=None):
         if additional_params is None:
             additional_params = {}
 
-        custom_fields_map = self.build_custom_fields_map(parent_id_list=format_values)
+        custom_fields_map, dropdown_fields_map = self.build_custom_fields_maps(parent_id_list=format_values)
 
         # Boards, Users, and Lists don't handle an api limit key
         # Passing in None doesn't change the response (no 400 returned)
@@ -220,7 +220,7 @@ class Stream:
             )
 
         for rec in records:
-            yield self.modify_record(rec, parent_id_list = format_values, custom_fields_map = custom_fields_map)
+            yield self.modify_record(rec, parent_id_list = format_values, custom_fields_map = custom_fields_map, dropdown_fields_map = dropdown_fields_map)
 
 
     def sync(self):
@@ -228,8 +228,9 @@ class Stream:
             yield rec
 
 class AddCustomFields(Mixin):
-    def build_custom_fields_map(self, **kwargs):
+    def build_custom_fields_maps(self, **kwargs):
         custom_fields_map = {}
+        dropdown_options_map = {}
         board_id_list = kwargs['parent_id_list']
         # The custom fields are defined on the board level, so this function is called on a per-board basis
         # Therefore, we assert that only one board is being passed in
@@ -237,13 +238,20 @@ class AddCustomFields(Mixin):
         custom_fields = self.client.get('/boards/{}/customFields'.format(board_id_list[0])) # pylint: disable=no-member
         for custom_field in custom_fields:
             custom_fields_map[custom_field['id']] = custom_field['name']
-        return custom_fields_map
+            if custom_field['type'] == 'list':
+                for dropdown_option in custom_field['options']:
+                    dropdown_options_map[dropdown_option['id']] = dropdown_option['value']['text']
+
+        return custom_fields_map, dropdown_options_map
 
 
     def modify_record(self, record, **kwargs): # pylint: disable=no-self-use
         custom_fields_map = kwargs['custom_fields_map']
+        dropdown_fields_map = kwargs['dropdown_fields_map']
         for custom_field in record['customFieldItems']:
             custom_field['name'] = custom_fields_map[custom_field['idCustomField']]
+            if custom_field.get('idValue', None):
+                custom_field['value'] = {'option': dropdown_fields_map[custom_field['idValue']]}
         return record
 
 
