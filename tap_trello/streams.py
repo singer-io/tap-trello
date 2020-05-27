cb0170d8221635b2005155755c1e305bd06d5ea9
Jacob Baca <52418765+jacobrobertbaca@users.noreply.github.com>
2020-05-27 09:20:25 -0400

    Trello/custom fields (#12)
    * rough draft of custom fields support
    
    * added support for custom fields
    
    * added docstring

--- tap_trello/streams.py
+++ tap_trello/streams.py
@@ -193,9 +193,14 @@ class Stream:
     def modify_record(self, record, **kwargs): # pylint: disable=no-self-use,unused-argument
         return record
 
+    def build_custom_fields_map(self, **kwargs): # pylint: disable=no-self-use,unused-argument
+        return {}
+
     def get_records(self, format_values, additional_params=None):
         if additional_params is None:
             additional_params = {}
+
+        custom_fields_map = self.build_custom_fields_map(parent_id_list=format_values)
 
         # Boards, Users, and Lists don't handle an api limit key
         # Passing in None doesn't change the response (no 400 returned)
@@ -215,12 +220,33 @@ class Stream:
             )
 
         for rec in records:
-            yield self.modify_record(rec, parent_id_list = format_values)
+            yield self.modify_record(rec, parent_id_list = format_values, custom_fields_map = custom_fields_map)
 
 
     def sync(self):
         for rec in self.get_records(self.get_format_values()):
             yield rec
+
+class AddCustomFields(Mixin):
+    def build_custom_fields_map(self, **kwargs):
+        custom_fields_map = {}
+        board_id_list = kwargs['parent_id_list']
+        # The custom fields are defined on the board level, so this function is called on a per-board basis
+        # Therefore, we assert that only one board is being passed in
+        assert len(board_id_list) == 1
+        custom_fields = self.client.get('/boards/{}/customFields'.format(board_id_list[0])) # pylint: disable=no-member
+        for custom_field in custom_fields:
+            custom_fields_map[custom_field['id']] = custom_field['name']
+        return custom_fields_map
+
+
+    def modify_record(self, record, **kwargs): # pylint: disable=no-self-use
+        custom_fields_map = kwargs['custom_fields_map']
+        for custom_field in record['customFieldItems']:
+            custom_field['name'] = custom_fields_map[custom_field['idCustomField']]
+        return record
+
+
 
 class AddBoardId(Mixin):
     def modify_record(self, record, **kwargs): # pylint: disable=no-self-use
@@ -324,7 +350,7 @@ class Actions(DateWindowPaginated, ChildStream):
     MAX_API_RESPONSE_SIZE = 1000
     params = {'limit': 1000}
 
-class Cards(ChildStream):
+class Cards(AddCustomFields, ChildStream):
     stream_id = "cards"
     stream_name = "cards"
     endpoint = "/boards/{}/cards/all"
@@ -332,7 +358,7 @@ class Cards(ChildStream):
     replication_method = "FULL_TABLE"
     parent_class = Boards
     MAX_API_RESPONSE_SIZE = 20000
-    params = {'limit': 20000}
+    params = {'limit': 20000, 'customFieldItems': 'true'}
 
 class Checklists(ChildStream):
     stream_id = "checklists"
