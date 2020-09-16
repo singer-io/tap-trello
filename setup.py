488cc87b8e1cb3ac1a46e5a53d6b8bea3d0a3aa1
Andy Lu <andy@stitchdata.com>
2020-09-16 14:18:29 -0500

    GA tap-trello (#21)
    * Bump version to v1.0.0, update changelog
    
    * Add to schema to get tests passing
    
    * Loosen asserts

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-trello",
-    version="0.1.0",
+    version="1.0.0",
     description="Singer.io tap for extracting data from Trello API",
     author="Stitch",
     url="http://singer.io",
